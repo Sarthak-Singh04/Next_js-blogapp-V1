daefaea9e23572541ef457558aedd6c89b3a0530
Sarthakparmar-04 <singhsarthak691@gmail.com>
2023-06-14 20:17:48 +0530

    make a portfolio image

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from "./page.module.css"
+import Image from 'next/image'
+import Button from '@/components/Button'
 
 function about() {
   return (
-    <div>about</div>
+    <div className={styles.container}>
+      <div className={styles.imgcontainer}>
+        <Image src="https://images.pexels.com/photos/2205683/pexels-photo-2205683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" fill={true} alt='cover image' className={styles.img}  />
+        <div className={styles.imgText}>
+        <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+        <p className={styles.imgdesc}>Handcrafting award winning digital experiences</p>
+
+
+        </div>
+      </div>
+      <div>
+        <div className={styles.textContainer}>
+          <div className={styles.item}>
+          <h1 className={styles.title}>Who are we</h1>
+          <p className={styles.desc}>The door or or gaunt bird. That this croaking bird on of within beast chamber bore, he nepenthe whispered quaff gently morrow a that borrow, from thy or in ah.</p>
+          <p className={styles.desc}>The door or or gaunt bird. That this croaking bird on of within beast chamber bore, he nepenthe whispered quaff gently morrow a that borrow, from thy or in ah.</p>
+
+          </div>
+          <div className={styles.item}>
+
+          <h1 className={styles.title}>What we do?</h1>
+          <p>At be name a that volume suddenly name, at mortals nothing <br></br> i be nights now thing i i. December tapping.</p>
+          <p className={styles.desc}>The door or or gaunt bird. That this croaking bird on of within beast chamber bore, he nepenthe whispered quaff gently morrow a that borrow, from thy or in ah.</p>
+          <Button url="/contact" text="Contact"/>
+
+          </div>
+
+
+      
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/hero.png'
+import Button from '@/components/Button'
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
     <div className={styles.item}>
       <h1 className={styles.title}>Let's Design something Innovative</h1>
       <p className={styles.desc}>Turng any idea into Reality.We bring together the teams from the the gloabal tech</p>
-      <button className={styles.button}>See our Works</button>
+      <Button url="/portfolio" text="See our works" />
     </div>
     <div className={styles.item}>
 
