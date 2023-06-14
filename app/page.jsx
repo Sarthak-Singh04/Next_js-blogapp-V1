45884f40a2511825b69acd4191ed72543b140da5
Sarthakparmar-04 <singhsarthak691@gmail.com>
2023-06-14 14:13:39 +0530

    design a home sction

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Hero from 'public/hero.png'
 export default function Home() {
   return (
    
-    <div>
+    <div className={styles.container}>
     <div className={styles.item}>
       <h1 className={styles.title}>Let's Design something Innovative</h1>
       <p className={styles.desc}>Turng any idea into Reality.We bring together the teams from the the gloabal tech</p>
